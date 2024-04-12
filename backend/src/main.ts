853cae68919a70b7bd8bdea1b476b35144ee0083
Abhijith H R <abhijith.hr@ib-systems.org>
2024-04-12 12:01:28 +0200

    added dockerfile

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.use(cors({
+    origin: [process.env.CORS_ORIGIN]
+  }));
+  await app.listen(4010);
 }
 bootstrap();
